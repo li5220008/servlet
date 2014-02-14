53a00025183491079fd11a256dec9067027bb1ee
free <li5220008@163.com>
2014-02-14 17:57:22 +0800

    servlet

---
+++ src/HelloServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * Desc:
+ * User: weiguili(li5220008@gmail.com)
+ * Date: 14-2-14
+ * Time: 下午1:54
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("<marquee>"+new Date()+"</marquee>");
+        out.println("<marquee>"+new Date()+"</marquee>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
